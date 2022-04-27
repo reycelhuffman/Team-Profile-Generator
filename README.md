fdb8c303603c48492733a816a0e802692afaefcd
Reycel Huffman <reycel.huffman@outlook.com>
2022-04-26 22:45:59 -0500

    created a readme

---
+++ README.md
@@ -0,0 +1,22 @@
+
+## Title Team Generator
+## Description: 
+This app is created in order for user to generate new employees profile, using inquirer module from node.js. This generates html page that's attached to the css.  
+## Video: 
+[![Demo Video](https://img.youtube.com/vi/LePBDVUQ-KY/0.jpg)](https://www.youtube.com/watch?v=LePBDVUQ-KY)
+
+## Installation:
+The user should clone the repository from GitHub and download node.  You need to have a file system and jest downloaded as well. 
+## Usage:
+The user will be prompt with questions that they need to answer. 
+## Credits:
+Dane Edwards, Andrew Layendecker
+## License: 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+This project is license under MIT
+## Contribute:
+Please how to install and make sure you follow through steps. 
+## Test:
+Run npm i, npm test and to run jest. 
+    
--- starter/index.js
+++ starter/index.js
@@ -3,104 +3,105 @@ const fs = require("fs")
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const Employee = require("./lib/Employee");
 
 
 
-//Engineer function
-const Employees = [];
-function addMembers() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter name here",
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your id number?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?',
-        },
-        {
-            type: 'input',
-            name: 'gitHub',
-            message: 'What is your gitHub?',
-        },
-        {
-            type: 'checkbox',
-            name: "role",
-            message: "What is your role?",
-            choices: [
-                "engineer",
-                "intern",
-                "manager",
-            ]
-        },
-        {
-            type: 'input',
-            name: 'addMembers',
-            message: 'Do you wanna add more members?',
-            choices: [
-                "yes",
-                "no"
-            ],
+    //Manager function
+    const promptUser= () => {
+        return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: "Enter name here",
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: 'What is your id number?'
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'What is your email?',
+                },
+                {
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: 'What is your officeNumber?',
+                },
+                {
+                    type: 'list',
+                    name: "role",
+                    message: "What is your role?",
+                    choices: [
+                    "engineer",
+                    "intern",
+                    "manager",  
+                ] 
+                }, 
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: 'Where do you go to school?',
+                },
+                {
+                    type: 'input',
+                    name: 'gitHub',
+                    message: 'What is your gitHub email?',
+                }
+            ])
+            .then((response) => {
+                console.log (response); 
+                let Employee = new Employee(response.name, response.id,response.email,response.role, response.officeNumber, response.school, response.gitHub)
+                const generateHTML = ({ response.name, response.id, response.email, response.officeNumber, response.role, response.school, response.gitHub }) =>
+                `<!DOCTYPE html>
+                <html lang="en">
+                
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Team Generator</title>
+                    <link rel="stylesheet" href="./starter/dist/styles.css">
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+                </head>
+                
+                <body>
+                    <header class="headerTitle">My Team</header>
+                    <section class="flexBoxTop">
+                        <section class="topCards">
+                            <div class="card-group">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Name ${response.name}</h4>
+                                        <h5 class="card-title">Role ${response.role}</h5>
+                                        <ul class="list">
+                                            <li class="listItems">ID ${response.id}</li>
+                                            <li class="listItems">Email${response.email}</li>
+                                            <li class="listItems">Office Number${response.officeNumber}</li>
+                                            <li class="listItems">School${response.school}</li>
+                                            <li class="listItems">gitHub${response.gitHub}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>    
+                        </section>
+                    </section>
+                </body>
+                </html>`;
+            })
         }
-    ])
-    function ask() {
-        inquirer.prompt(question).then((answers) => {
-            output.push(answers.name);
-        }
-        )
-    }
 
-
-    const generateHTML = ({ name, id, email, gitHub, officeNumber, school, role }) =>
-        `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team Generator</title>
-        <link rel="stylesheet" href="./starter/dist/styles.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    </head>
-    
-    <body>
-        <header class="headerTitle">My Team</header>
-        <section class="flexBoxTop">
-            <section class="topCards">
-                <div class="card-group">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">Name ${name}</h4>
-                            <h5 class="card-title">Role ${role}</h5>
-                            <ul class="list">
-                                <li class="listItems">ID ${id}</li>
-                                <li class="listItems">Email${email}</li>
-                                <li class="listItems">Office Number${officeNumber}</li>
-                                <li class="listItems">School${school}</li>
-                                <li class="listItems">gitHub${gitHub}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>    
-            </section>
-        </section>
-    </body>
-    </html>`;
 
     const init = () => {
-        promptAddMembers()
-            // Use writeFileSync method to use promises instead of a callback function
-            .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-            .then(() => console.log('Successfully wrote to index.html'))
-            .catch((err) => console.error(err));
+        promptUser()
+          // Use writeFileSync method to use promises instead of a callback function
+        .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+        .then(() => console.log('Successfully wrote to index.html'))
+        .catch((err) => console.error(err));
     };
+    
+    init();
 
-    init(); 
+    
